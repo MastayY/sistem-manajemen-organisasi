40f8d5a9f6b18be6ce0520db9536fb5e81c4b87a
Mastay <nasywanfadilah@gmail.com>
2025-05-06 15:09:02 +0700

    feat: landing page progress (navbar & hero section)

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -38,13 +38,16 @@
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=instrument-sans:400,500,600" rel="stylesheet" />
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
 
         @routes
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/pages/{$page['component']}.tsx"])
         @inertiaHead
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-jakarta antialiased">
         @inertia
     </body>
 </html>
